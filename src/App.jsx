bf84467fdf87faa7845a563b064e3b4aeac8731d
KABILESHWARAN B <kabileshwaran183@gmail.com>
2024-12-12 16:03:34 +0530

    First React App
    uploaded via github desktop

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar"
+import MainComponent from "./MainComponent"
+function App() {
+  return(
+    <>
+      <Navbar />
+      <MainComponent />
+    </>
+  )
+  
+}
+
+export default App
